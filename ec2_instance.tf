6d781c426e760cd738a511109783cfbbcf3fad35
Nimendra Gunawardana <nimendraamiththa1@gmail.com>
2025-04-01 03:20:45 +0530

    removed secret keys

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "eu-north-1"
-  access_key = var.AKIAYQNJS22XRJLXE27A
-  secret_key = var.Ag8NBZzSSMwHNGKTjCMuffQdb+msJI/ARbJtJzK5
 }
 
 resource "aws_instance" "bookbuddies_server" {
